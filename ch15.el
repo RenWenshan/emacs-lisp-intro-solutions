c15a77cfd2eccbe96bd31b652220ac67130b909c
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2013-03-27 13:31:36 -0400

    Ch. 15: Prefix functions and give example output

--- ch15.el
+++ ch15.el
@@ -31,20 +31,20 @@
 ;;
 ;; Solution:
 
-(defvar graph-symbol "*"
+(defvar wenshan-graph-symbol "*"
   "String used as symbol in graph, usually an asterisk.")
 
-(defvar graph-blank " "
+(defvar wenshan-graph-blank " "
   "String used as blank in graph, usually a blank
-space. graph-blank must be the same number of columns wide as
-graph-symbol")
+space. wenshan-graph-blank must be the same number of columns wide as
+wenshan-graph-symbol")
 
-(defun column-of-graph (max-graph-height actual-height &optional line)
+(defun wenshan-column-of-graph (max-graph-height actual-height &optional line)
   "Return MAX-GRAPH-HEIGHT strings; ACTUAL-HEIGHT are graph-symbols.
 The graph-symbols are contiguous entries at the end
 of the list.
 The list will be inserted as one column of a graph.
-The strings are either graph-blank or graph-symbol.
+The strings are either wenshan-graph-blank or wenshan-graph-symbol.
 
 The optional argument `line' will be used to determine whether to
 draw bar graph or line graph. (By default, bar graph)"
@@ -56,22 +56,22 @@ draw bar graph or line graph. (By default, bar graph)"
     (if line
         ;; line graph
         (progn
-         ;; Fill in `graph-blanks'.
+         ;; Fill in `wenshan-graph-blanks'.
          (while (> actual-height 1)
-           (setq insert-list (cons graph-blank insert-list))
+           (setq insert-list (cons wenshan-graph-blank insert-list))
            (setq actual-height (1- actual-height)))
-         ;; Fill in `graph-symbol'
-         (setq insert-list (cons graph-symbol insert-list)))
+         ;; Fill in `wenshan-graph-symbol'
+         (setq insert-list (cons wenshan-graph-symbol insert-list)))
 
       ;; bar graph
-      ;;Fill in `graph-symbols'.
+      ;;Fill in `wenshan-graph-symbols'.
       (while (> actual-height 0)
-        (setq insert-list (cons graph-symbol insert-list))
+        (setq insert-list (cons wenshan-graph-symbol insert-list))
         (setq actual-height (1- actual-height))))
 
-    ;; Fill in `graph-blanks'.
+    ;; Fill in `wenshan-graph-blanks'.
     (while (> number-of-top-blanks 0)
-      (setq insert-list (cons graph-blank insert-list))
+      (setq insert-list (cons wenshan-graph-blank insert-list))
       (setq number-of-top-blanks
             (1- number-of-top-blanks)))
 
@@ -79,20 +79,20 @@ draw bar graph or line graph. (By default, bar graph)"
     insert-list))
 
 
-(defun graph-body-print (numbers-list &optional line)
+(defun wenshan-graph-body-print (numbers-list &optional line)
   "Print a bar graph or line graph of the NUMBERS-LIST.
 The numbers-list consists of the Y-axis values.
 
 `line': if true, print line graph; if nil, print bar graph"
 
   (let ((height (apply 'max numbers-list))
-        (symbol-width (length graph-blank))
+        (symbol-width (length wenshan-graph-blank))
         from-position)
 
     (while numbers-list
       (setq from-position (point))
       (insert-rectangle
-       (column-of-graph height (car numbers-list) line))
+       (wenshan-column-of-graph height (car numbers-list) line))
       (goto-char from-position)
       (forward-char symbol-width)
       ;; Draw graph column by column.
@@ -112,7 +112,7 @@ The numbers-list consists of the Y-axis values.
 
 `line': if true, print line graph; if nil, print bar graph"
   (let ((height (apply 'max numbers-list))
-        (symbol-width (length graph-blank))
+        (symbol-width (length wenshan-graph-blank))
         from-position)
     (recursive-graph-body-print-internal
      numbers-list
@@ -132,9 +132,11 @@ draw bar graph or line graph. (By default, bar graph)"
   (when numbers-list
     (setq from-position (point))
     (insert-rectangle
-     (column-of-graph height (car numbers-list) line))
+     (wenshan-column-of-graph height (car numbers-list) line))
     (goto-char from-position)
     (forward-char symbol-width)
     (sit-for 0)     ; Draw graph column by column.
     (recursive-graph-body-print-internal
      (cdr numbers-list) height symbol-width line)))
+
+(wenshan-graph-body-print '(1 2 3 4 6 4 3 5 7 6 5 2 3) t)
